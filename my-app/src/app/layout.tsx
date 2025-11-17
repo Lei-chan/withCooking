2e3f781eed6a80d02b899f2c825c7642a3cb41ca
Lei-chan <hirosebirosepirose01@yahoo.co.jp>
2025-11-17 09:21:39 +0900

    Updated

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,18 +1,25 @@
 import type { Metadata } from "next";
+import type { Viewport } from "next";
 import { Playfair_Display, Noto_Serif_JP } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./lib/providers";
+
+export const metadata: Metadata = {
+  title: "withCooking",
+  description: "Application where users can use many useful tools for cooking!",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+};
 
 const playfairDisplay = Playfair_Display({
   weight: "400",
   subsets: ["latin"],
   fallback: ["Noto Serif JP", "serif"],
 });
-
-export const metadata: Metadata = {
-  title: "withCooking",
-  description: "Application where users can use many useful tools for cooking!",
-};
 
 export default function RootLayout({
   children,
